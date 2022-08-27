d7839373cc17e772a28ae154e2b2a4dd3d60ba1c
san510 <47128979+san510@users.noreply.github.com>
2022-08-27 12:34:45 +0900

    10.レンダリング最適化(memo)

--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,10 +1,13 @@
+import { memo } from "react";
+
 const style = {
   width: "100%",
   height: "200px",
   backgroundColor: "khaki"
 };
 
-export const ChildArea = (props) => {
+// memo: propsが変更されない限り再レンダリングされない
+export const ChildArea = memo((props) => {
   const { open } = props;
   console.log("ChildAreaがレンダリングされた!!");
 
@@ -22,4 +25,4 @@ export const ChildArea = (props) => {
       ) : null}
     </>
   );
-};
+});
